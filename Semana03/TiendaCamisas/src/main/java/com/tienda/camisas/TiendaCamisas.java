7b2268fa39475ba23cb3a3d2d46943f8d4f2ce65
Estebangi11 <t01288g@ms.upla.edu.pe>
2025-09-22 04:41:59 -0500

    Add files via upload

---
+++ Semana03/TiendaCamisas/src/main/java/com/tienda/camisas/TiendaCamisas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.tienda.camisas;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class TiendaCamisas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
