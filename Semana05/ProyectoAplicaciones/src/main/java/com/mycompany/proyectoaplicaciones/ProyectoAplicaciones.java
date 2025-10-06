52a9b3a1841171fabffd0a963e5bd6c935cf96aa
Estebangi11 <t01288g@ms.upla.edu.pe>
2025-10-05 22:34:32 -0500

    Add files via upload

---
+++ Semana05/ProyectoAplicaciones/src/main/java/com/mycompany/proyectoaplicaciones/ProyectoAplicaciones.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectoaplicaciones;
+
+import vista.login;
+
+/**
+ *
+ * @author WindowsPC
+ */
+public class ProyectoAplicaciones {
+
+    public static void main(String[] args) {
+        login pension =new login();
+        pension.setVisible(true);
+    }
+}
